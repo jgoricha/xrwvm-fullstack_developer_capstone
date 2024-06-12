ed49bfbd01ed78d1401c1129e552fc4c1180ff53
jgoricha <janvigoricha@gmail.com>
2024-06-12 10:38:06 -0400

    week 3 still bugs

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,6 +19,7 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
