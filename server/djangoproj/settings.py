09674f1928866f7899d55486216b34063da7b4dd
jgoricha <92893208+jgoricha@users.noreply.github.com>
2024-06-14 12:25:53 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-    'https://janvigoricha-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://janvigoricha-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 """https://janvigoricha-8000.theiadockernext-0-labs-
+                    prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"""]
+CSRF_TRUSTED_ORIGINS = ["""https://janvigoricha-8000.theiadockernext-0-
+                           labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"""]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
