c80c89d5ab339617e7ca0c035fdb20ff5d5df3db
jgoricha <92893208+jgoricha@users.noreply.github.com>
2024-06-14 12:32:17 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,8 +31,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost',
                  """https://janvigoricha-8000.theiadockernext-0-labs-
                     prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"""]
-CSRF_TRUSTED_ORIGINS = ["""https://janvigoricha-8000.theiadockernext-0-
-                           labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"""]
+CSRF_TRUSTED_ORIGINS = ["""https://janvigoricha-8000.theiadockernext-0-labs
+                           -prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"""]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -96,9 +96,11 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
+    }
     {
         'NAME':
         'django.contrib.auth.password_validation.MinimumLengthValidator',
